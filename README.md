29d74a918224371d184f61423bf0218c78042ccf
Stève Oriol <steve@cdg38.fr>
2021-03-26 13:56:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Script-multi-core-Drupal
+
+Modifier le fichier "sites.txt" en renseignant le chemin de la racine de chaque site à traiter avec le script, un seul site par ligne.
+
+Puis lancez le script en ajoutant la commande à exécuter en paramètre, exemples :
+ ./exeSites.sh "drush status"
+ ./exeSites.sh "composer require drupal/token"
---
+++ exeSites.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+filename=$DIR/sites.txt
+DORIGINE=`pwd`
+echo "#####################################################"
+echo "#  Script de gestion de plusieurs instances drupal  #"
+echo "#                 [ version 0.1 ]                   #"
+echo "#####################################################"
+echo ""
+echo "Liste les instances traitées (Ajouter les dossiers racines dans le fichier $filename) : "
+while IFS= read -r dpath; 
+do
+	echo ">>$dpath<<";
+done < $filename
+echo ""
+
+if test "$1" == "" 
+   then 
+     echo "Il manque la commande à exécuter ;-)" 
+     echo "Exemples : exeSites.sh \"drush status\""
+     echo "           exeSites.sh \"composer require drupal/coffee:1.x-dev@dev\""
+     exit 1 
+   else
+     echo "la commande demander est : $1"
+fi
+
+getConfirmContinue() {
+    local REPONSE=""
+
+    echo -e "Confirmez ? (o/n) : \c"
+    read REPONSE
+    if test "$REPONSE" != "o" -a "$REPONSE" != "O"
+    then
+        echo "(reponse = non)"
+        exit 1
+    else
+        return 0
+    fi
+}
+getConfirmContinue
+
+
+echo Début:
+
+while IFS= read -r dpath; 
+do 
+	echo ""
+	echo ">>$dpath<<";
+	if  cd $dpath 
+	then
+		$1	
+	else
+        	echo "Rien à faire ici ..."
+	fi
+
+done < $filename
+
+cd $DORIGINE
+echo Fin..
